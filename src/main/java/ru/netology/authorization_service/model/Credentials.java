df76aad7907e484afd56b450c4c46994de2256f3
Vitaliy Murzin <murzinvit@yandex.ru>
2024-10-08 02:49:23 +0500

    fix repository, add model

---
+++ src/main/java/ru/netology/authorization_service/model/Credentials.java
@@ -0,0 +1,27 @@
+package ru.netology.authorization_service.model;
+
+public class Credentials {
+    private String username;
+    private String password;
+
+    public Credentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean equals(Credentials credentials) {
+        if(credentials.getUsername().equals(this.username) && credentials.getPassword().equals(this.password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- src/main/java/ru/netology/authorization_service/repository/UserRepository.java
+++ src/main/java/ru/netology/authorization_service/repository/UserRepository.java
@@ -1,16 +1,25 @@
 package ru.netology.authorization_service.repository;
 
 import org.springframework.stereotype.Repository;
+import ru.netology.authorization_service.model.Credentials;
 import ru.netology.authorization_service.permissions.Authorities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class UserRepository {
-    private List<Authorities> authoritiesList = new ArrayList<>();
+    private ConcurrentHashMap<Credentials, Authorities> concurrentHashMap = new ConcurrentHashMap<>();
 
     public List<Authorities> getUserAuthorities(String user, String password) {
-        return List.of(Authorities.values());
+        Credentials credentials = new Credentials(user, password);
+        for (Map.Entry<Credentials, Authorities> entryMap : concurrentHashMap.entrySet()) {
+            if (entryMap.getKey().equals(credentials)) {
+                return List.of(entryMap.getValue());
+            }
+        }
+        return new ArrayList<Authorities>();
     }
 }
